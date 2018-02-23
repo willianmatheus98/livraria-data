e26fef51c672985dba6b8165246d046493415bab
wilaman21 <williantecnico1998@gmail.com>
2018-02-22 21:20:43 -0300

    v2

--- livraria/src/main/java/br/edu/unichristus/Livro.java
+++ livraria/src/main/java/br/edu/unichristus/Livro.java
@@ -13,23 +13,23 @@ import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name="TB_LIVRO")
+@Table(name = "TB_LIVRO")
 @Data
 public class Livro {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "LIVRO_ID")
 	private Long livroID;
-	
+
 	@Column(name = "TITULO")
 	private String titulo;
-	
+
 	@Column(name = "DATA_PUBLICACAO")
 	private Date dataPublicacao;
-	
+
 	@Column(name = "NUMERO_PAGINAS")
 	private int numeroPaginas;
-	
+
 	@Column(name = "PRECO")
 	private BigDecimal preco;
 
@@ -40,6 +40,52 @@ public class Livro {
 		this.titulo = titulo;
 		this.dataPublicacao = dataPublicacao;
 		this.numeroPaginas = numeroPaginas;
+		this.preco = preco;
+	}
+
+	@Override
+	public String toString() {
+		return "Livro [livroId=" + livroID + ", título=" + titulo + ", data de	publicação=" + dataPublicacao
+				+ ", número de Páginas=" + numeroPaginas + ", preço=" + preco + "]";
+	}
+
+	public Long getLivroID() {
+		return livroID;
+	}
+
+	public void setLivroID(Long livroID) {
+		this.livroID = livroID;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Date getDataPublicacao() {
+		return dataPublicacao;
+	}
+
+	public void setDataPublicacao(Date dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+
+	public int getNumeroPaginas() {
+		return numeroPaginas;
+	}
+
+	public void setNumeroPaginas(int numeroPaginas) {
+		this.numeroPaginas = numeroPaginas;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
 
